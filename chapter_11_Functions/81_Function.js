cc1359b6eb606c5e8cb7f1a81559c859f331ff14
Diwakar-dotcom <dky2713@gmail.com>
2026-08-02 17:17:27 +0530

    functions

---
+++ chapter_11_Functions/81_Function.js
@@ -0,0 +1,18 @@
+// Without function - repeated logic
+
+let score1 = 85;
+let result1 = score1 >= 70 ? "pass":"fail";
+console.log(result1);
+
+let score2 = 45;
+let result2 = score2 >= 70 ? "pass":"fail";
+console.log(result2);
+
+//Define
+function getResult(score) {
+    return score >= 70 ? "pass":"fail";
+}
+
+// Calling
+getResult(50); // fail
+getResult(80); // pass
