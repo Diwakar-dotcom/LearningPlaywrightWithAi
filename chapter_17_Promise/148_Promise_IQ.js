4e27fd18f5aba0344b675578896ea68860f077ad
Diwakar-dotcom <dky2713@gmail.com>
2026-09-03 08:17:45 +0530

    promise

--- chapter_17_Promise/148_Promise_IQ.js
+++ chapter_17_Promise/148_Promise_IQ.js
@@ -13,3 +13,44 @@ let q = new Promise(function(resolve, reject){
 q.catch(function(err){
     console.log("Caught:", err);
 })
+
+let r = Promise.resolve(5);
+
+r.then(function(val){
+    return val*10;
+}).then(function(val){
+    console.log("Result:", val);
+});
+
+Promise.resolve(1)
+.then(function(val){
+    console.log(val);
+    return val+1;
+}).then(function(val){
+    console.log(val);
+    return val+1;
+}).then(function(val){
+    console.log(val);
+    return val+1;
+}).then(function(val){
+    console.log(val);
+})
+
+Promise.resolve("Start")
+.then(function(val){
+    console.log(val);
+    throw new Error("Broke at step 2");
+}).then(function(){
+    console.log("This will not run");
+}).catch(function(err){
+    console.log("Caught: ",err.message);
+})
+
+Promise.reject("Test Failed")
+.then(function(data){
+    console.log("Data:", data);
+}).catch(function(err){
+    console.log("Error:", err.message);
+}).finally(function(){
+    console.log("Cleanup done")
+});
